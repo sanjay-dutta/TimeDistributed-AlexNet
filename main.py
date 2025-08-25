3ff2eea28bd332a9a8afeaa8e8412de2622f2166
Dr. Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2025-08-25 22:47:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from model import create_model
 from train import train_model
 from evaluate import evaluate_model
 
-DATASET_DIR = "/impacs/sad64/SLURM/dataset/ClassroomActivity"
+DATASET_DIR = "Dataset path"
 
 train_gen, test_gen = get_generators(DATASET_DIR)
 model = create_model(25, 224, 224, 10)
